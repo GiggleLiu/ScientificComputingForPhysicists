1ff88be2cf010cf18dedbe3185b20544af3243e3
Yusheng Zhao <yushengzhao2020@outlook.com>
2024-01-31 17:44:37 +0800

    abort running code

--- src/BookTemplate.jl
+++ src/BookTemplate.jl
@@ -32,7 +32,7 @@ function build()
 	println("Building your awesome book!")
 	# To avoid publishing broken websites.
 	fail_on_error = true
-	gen(; fail_on_error)
+	# gen(; fail_on_error)
 	build_all(; fail_on_error)
 end
 

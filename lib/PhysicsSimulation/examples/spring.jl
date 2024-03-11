b7a02ca9fa9e01f2acc5de5b2786ec1aade7fe2b
GiggleLiu <cacate0129@gmail.com>
2024-03-11 09:57:55 +0800

    update

--- lib/PhysicsSimulation/examples/spring.jl
+++ lib/PhysicsSimulation/examples/spring.jl
@@ -2,8 +2,12 @@ using Makie: RGBA
 using Makie, CairoMakie
 using PhysicsSimulation
 
-spring = spring_chain(0.2 * randn(20), 3.0, 1.0; periodic=true)
+C, M = 3.0, 1.0
+L = 20
+u0 = 0.2 * randn(L)
+spring = spring_chain(u0, C, M; periodic=true)
 states = leapfrog_simulation(spring; dt=0.1, nsteps=500)
+states_exact = waveat(eigenmodes(eigensystem(spring)), u0, 0.1 * (0:500))
 
 # visualize the system
 p2(x::PhysicsSimulation.Point{1}) = Point2f(x.data[1], 0.0)
@@ -11,17 +15,22 @@ getcoos(b::LeapFrogSystem) = p2.(coordinate(b.sys))
 getendpoints(b::LeapFrogSystem) = p2.(b.a)
 
 locs = getcoos.(states)
+locs2 = [map(x->Point2f(x, -0.05), (0:L-1) .+ x) for x in states_exact]
 vecs = getendpoints.(states)
 
 fig = Figure()
-ax = Axis(fig[1, 1])
-coos = Observable(locs[1])
+ax1 = Axis(fig[1, 1]; limits=(-1, length(locs[1]), -0.4, 0.3))
+coos1 = Observable(locs[1])
+coos2 = Observable(locs2[1])
 endpoints = Observable(vecs[1])
-scatter!(ax, coos, markersize = 10, color = :blue, limits = (-1, length(locs[1]), -1, 1))
-arrows!(ax, coos, endpoints; color = :red)
+scatter!(ax1, coos1, markersize = 10, color = :blue, limits = (-1, length(locs[1]), -1, 1), label="simulation")
+arrows!(ax1, coos1, endpoints; color = :red)
+scatter!(ax1, coos2, markersize = 10, color = :cyan, limits = (-1, length(locs[1]), -1, 1), label="exact")
+axislegend(ax1)
 
 record(fig, joinpath(@__DIR__, "springs-simulate.mp4"), 2:length(states); framerate = 24) do i
-    coos[] = locs[i]
+    coos1[] = locs[i]
+    coos2[] = locs2[i]
     endpoints[] = vecs[i]
 end
 
